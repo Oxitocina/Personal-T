6f59e4a43bc724a748dba8152e9cc312ada1f99f
isidoro <isidoro@isidoro-N55SF.(none)>
2014-04-03 12:39:53 +0200

    Primera version completamente funcional

--- PTrainer.c
+++ PTrainer.c
@@ -8,8 +8,19 @@ int eligeTrabajo(int, int);
 int main (int argc, char *argv[]){
 	srand(time(NULL));
 	int contador=0;
+	int ultimos[2];
+	ultimos[0]=0;
+	ultimos[1]=0;
 	while (1){
 		int numero= rand()%10;
+		if (numero == ultimos[0] || numero == ultimos[1])
+			continue;
+		else {
+			if(contador%2==0)
+				ultimos[0]=numero;
+			else
+				ultimos[1]=numero;
+		}
 		contador=eligeTrabajo(numero, contador);
 		if (contador==0)
 			break;
@@ -22,69 +33,71 @@ int eligeTrabajo(int numero, int contador){
 	int counter=contador+1;
 	switch (numero){
 		case 1:
-			printf("30 flexiones soldado. ¡AHORA!");
-			printf("Terminaste. Pulsa intro para tu siguiente trabajo");
+			printf("30 flexiones soldado. ¡AHORA!\n");
+			printf("Terminaste. Pulsa intro para tu siguiente trabajo\n");
+			getchar();
 			break;
 		case 2:
-			printf("25 sentadillas soldado. ¡AHORA!");
-			printf("Terminaste. Pulsa intro para tu siguiente trabajo");
-
+			printf("25 sentadillas soldado. ¡AHORA!\n");
+			printf("Terminaste. Pulsa intro para tu siguiente trabajo\n");
+			getchar();
 			break;
 		case 3:
-			printf("30 abdominales superiores soldado. ¡AHORA!");
-			printf("Terminaste. Pulsa intro para tu siguiente trabajo");
-
+			printf("30 abdominales superiores soldado. ¡AHORA!\n");
+			printf("Terminaste. Pulsa intro para tu siguiente trabajo\n");
+			getchar();
 			break;
 		case 4:
-			printf("25 \"escalones\" soldado. ¡AHORA!");
-			printf("Terminaste. Pulsa intro para tu siguiente trabajo");
-
+			printf("25 \"escalones\" soldado. ¡AHORA!\n");
+			printf("Terminaste. Pulsa intro para tu siguiente trabajo\n");
+			getchar();
 			break;
 		case 5:
-			printf("25 zancadas soldado. ¡AHORA!");
-			printf("Terminaste. Pulsa intro para tu siguiente trabajo");
-
+			printf("25 zancadas soldado. ¡AHORA!\n");
+			printf("Terminaste. Pulsa intro para tu siguiente trabajo\n");
+			getchar();
 			break;
 		case 6:
-			printf("30 segundos de planking con codos soldado. ¡AHORA!");
+			printf("30 segundos de planking con codos soldado. ¡AHORA!\n");
 			cuentaAtras(30);
-			printf("Terminaste. Pulsa intro para tu siguiente trabajo");
-
+			printf("Terminaste. Pulsa intro para tu siguiente trabajo\n");
+			getchar();
 			break;
 		case 7:
-			printf("30 segundos de sentadillas contra la pared soldado. ¡AHORA!");
+			printf("30 segundos de sentadillas contra la pared soldado. ¡AHORA!\n");
 			cuentaAtras(30);
-			printf("Terminaste. Pulsa intro para tu siguiente trabajo");
-
+			printf("Terminaste. Pulsa intro para tu siguiente trabajo\n");
+			getchar();
 			break;
 		case 8:
-			printf("30 abdominales superiores soldado. ¡AHORA!");
-			printf("Terminaste. Pulsa intro para tu siguiente trabajo");
-
+			printf("30 abdominales superiores soldado. ¡AHORA!\n");
+			printf("Terminaste. Pulsa intro para tu siguiente trabajo\n");
+			getchar();
 			break;
 		case 9:
-			printf("30 lumbares soldado. ¡AHORA!");
-			printf("Terminaste. Pulsa intro para tu siguiente trabajo");
-
+			printf("30 lumbares soldado. ¡AHORA!\n");
+			printf("Terminaste. Pulsa intro para tu siguiente trabajo\n");
+			getchar();
 			break;
 		default:
 			if (counter<7)
 				return counter-1;
 			else{
-				printf("Buen trabajo soldado, descansa.");
+				printf("Buen trabajo soldado, descansa.\n");
+				getchar();
 				return 0;
 			}
 	}
 	return counter;
 }
 int cuentaAtras(int segundos){
-	printf("3");
+	printf("3\n");
 	sleep(1);
-	printf("2");
+	printf("2\n");
 	sleep(1);
-	printf("1");
-	sleep("1");
-	printf("¡YA!");
+	printf("1\n");
+	sleep(1);
+	printf("¡YA!\n");
 	sleep(segundos);
 	return 0;
 }
